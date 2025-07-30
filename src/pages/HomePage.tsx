0328a21c8e9c0827edffe1cf8191779d4a7208e3
HumedMohammed <humed@numuw.com>
2025-07-30 23:47:41 +0300

    feat: add Pocketbase integration and various loading components with animations

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -20,7 +20,6 @@ import {
   Award,
   ChevronDown,
 } from "lucide-react";
-import { Link } from "react-router-dom";
 import { Footer } from "@/components/Footer";
 import { Navbar } from "@/components/Navbar";
 
